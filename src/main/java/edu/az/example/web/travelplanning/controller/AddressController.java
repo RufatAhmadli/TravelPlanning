a2d7e98a6354916b522738346c6f3b501ccd4164
Rufat <ahmadlirufat10@gmail.com>
2025-10-17 00:26:51 +0400

    Updated AddressController

--- src/main/java/edu/az/example/web/travelplanning/controller/AddressController.java
+++ src/main/java/edu/az/example/web/travelplanning/controller/AddressController.java
@@ -7,6 +7,7 @@ import edu.az.example.web.travelplanning.service.AddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class AddressController {
     private final AddressService addressService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<AddressDto>> getAddresses() {
         return ResponseEntity.ok(addressService.findAll());
     }
@@ -27,21 +29,25 @@ public class AddressController {
         return ResponseEntity.ok(addressService.findById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/city/{city}")
     public ResponseEntity<List<AddressDto>> getAddressesByCity(@PathVariable String city) {
         return ResponseEntity.ok(addressService.findAllByCity(city));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/addressType/{addressType}")
     public ResponseEntity<List<AddressDto>> getAddressesByAddressType(@PathVariable AddressType addressType) {
         return ResponseEntity.ok(addressService.findAllByAddressType(addressType));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/street")
     public ResponseEntity<List<AddressDto>> getAllAddressesByStreet(@RequestParam String street, @RequestParam String number) {
         return ResponseEntity.ok(addressService.findAllByStreet(street, number));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/userId/{id}")
     @ResponseStatus(HttpStatus.OK)
     public List<AddressDto> getAddressesByUserId(@PathVariable Long id) {
@@ -49,7 +55,7 @@ public class AddressController {
     }
 
     @PostMapping
-    public ResponseEntity<AddressDto> createAddress(@RequestBody AddressDto addressDto) {
+    public ResponseEntity<AddressDto> createAddress(@RequestBody AddressDto addressDto)     {
         return new ResponseEntity<>(addressService.create(addressDto), HttpStatus.CREATED);
     }
 
