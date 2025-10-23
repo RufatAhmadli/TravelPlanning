65aa791761086d8620998eeca03c9ed98f5f3165
Rufat <ahmadlirufat10@gmail.com>
2025-10-23 18:34:23 +0400

    Created update feature

--- src/main/java/edu/az/example/web/travelplanning/mapper/ReviewMapper.java
+++ src/main/java/edu/az/example/web/travelplanning/mapper/ReviewMapper.java
@@ -8,6 +8,7 @@ import edu.az.example.web.travelplanning.model.entity.TripReview;
 import edu.az.example.web.travelplanning.model.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 
 @Mapper
@@ -19,17 +20,24 @@ public interface ReviewMapper {
 
     @Mapping(target = "trip", ignore = true)
     @Mapping(target = "user", ignore = true)
+    @Mapping(target = "id", ignore = true)
     TripReview toReviewEntity(ReviewDto reviewDto);
 
     @Mapping(target = "firstName", source = "name")
     @Mapping(target = "confirmPassword", ignore = true)
     @Mapping(target = "trips", ignore = true)
     @Mapping(target = "addresses", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
     @Named("userWithoutRelations")
     UserDto userWithoutAddressesAndTrips(User user);
 
     @Mapping(target = "reviews", ignore = true)
     @Named("tripWithoutRelations")
     TripDto mapTripWithoutRelations(Trip trip);
+
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "trip", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    TripReview updateReview(@MappingTarget TripReview tripReview, ReviewDto reviewDto);
 }
 
